d8c5b6b7a3d70428cda4652e60aafb97ad0f5b43
sunxiongkun <sunxiongkun@wepiao.com>
2016-07-20 14:45:38 +0800

    redisTemplate测试

---
+++ boot-redis/src/main/java/com/sxk/bootstrap/RedisConfiguration.java
@@ -0,0 +1,42 @@
+package com.sxk.bootstrap;
+
+import java.util.ResourceBundle;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import redis.clients.jedis.JedisShardInfo;
+
+@Configuration
+public class RedisConfiguration {
+
+    private static final ResourceBundle redisBundle = ResourceBundle.getBundle("redis");
+
+    @Bean
+    public StringRedisTemplate stringRredisTemplate() {
+        StringRedisTemplate template = new StringRedisTemplate(redisConnectionFactory());
+        // explicitly enable transaction support
+        //template.setEnableTransactionSupport(true);
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        String ip = redisBundle.getString("redis.server.0.ip");
+        int port = Integer.parseInt(redisBundle.getString("redis.server.0.port"));
+        JedisShardInfo shard = new JedisShardInfo(ip, port);
+        return new JedisConnectionFactory(shard);
+    }
+
+}
